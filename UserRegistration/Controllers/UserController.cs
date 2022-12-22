e3aa5bfbd7d33e4d60a8785a3d9fc62a8547e473
Lechieuduong <banbetui983@gmail.com>
2022-12-22 14:28:48 +0700

    Create Register app

---
+++ UserRegistration/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegistration.Models;
+
+namespace UserRegistration.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddOrEdit(int id=0)
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(User userModel)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                if (dbModel.Users.Any(x => x.Username == userModel.Username))
+                {
+                    ViewBag.DuplicateMessage = "Username already exist.";
+                    return View("AddOrEdit", userModel);
+                }
+
+                dbModel.Users.Add(userModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful.";
+            return View("AddOrEdit", new User());
+        }
+	}
+}
